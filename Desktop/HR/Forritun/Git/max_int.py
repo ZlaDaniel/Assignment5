987b9f4e6d05a8f31c6432c43698d1f64d5e46db
Daniel <danni539953@gmail.com>
2019-09-05 09:40:37 +0000

    max_int.py

---
+++ Desktop/HR/Forritun/Git/max_int.py
@@ -0,0 +1,8 @@
+max_int = 0
+while True:
+    num_int = int(input("Input a number: "))    # Do not change this line
+    if num_int >= max_int:
+        max_int = num_int
+    if num_int <= 0:
+        break
+print("The maximum is", max_int)    # Do not change this line
